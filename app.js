b818a6d8f0e7767d3c6b6e20df9c3a67d5e01ca0
Jordan Christensen <jkchristensen36@gmail.com>
2023-09-18 11:47:42 -0600

    FileSync drafts

---
+++ app.js
@@ -0,0 +1,5 @@
+const fs = require('fs')
+
+fs.writeFileSync('notes.txt', 'This file was created by Jordan!')
+// fs.appendFileSync('./notes.txt', ' This text was appended useing appendFileSync!')
+
